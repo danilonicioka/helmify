5a81d3864064ce54f603e100decccaf74f882bf6
danilo.nicioka <danilo.nicioka@tjpa.jus.br>
2026-06-02 17:01:28 -0300

    test: add unit tests for route processor

---
+++ pkg/processor/route/route_test.go
@@ -0,0 +1,76 @@
+package route
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/arttor/helmify/internal"
+	"github.com/arttor/helmify/pkg/metadata"
+	"github.com/stretchr/testify/assert"
+)
+
+const routeYaml = `apiVersion: route.openshift.io/v1
+kind: Route
+metadata:
+  name: my-app-api
+  namespace: my-namespace
+  annotations:
+    my-annotation: annotation-value
+spec:
+  host: my-app-api.apps.ocp-hub.i.tj.pa.gov.br
+  path: /api
+  to:
+    kind: Service
+    name: my-app-api-service
+  port:
+    targetPort: 8080
+  tls:
+    termination: edge
+    insecureEdgeTerminationPolicy: Redirect`
+
+func Test_route_Process(t *testing.T) {
+	var testInstance route
+
+	t.Run("processed", func(t *testing.T) {
+		obj := internal.GenerateObj(routeYaml)
+		processed, template, err := testInstance.Process(&metadata.Service{}, obj)
+		assert.NoError(t, err)
+		assert.Equal(t, true, processed)
+		assert.NotNil(t, template)
+
+		// Check values
+		values := template.Values()
+		assert.NotEmpty(t, values)
+
+		// Check if file content is writeable and has expected combined output
+		var buf bytes.Buffer
+		err = template.Write(&buf)
+		assert.NoError(t, err)
+		
+		content := buf.String()
+		assert.Contains(t, content, "apiVersion: route.openshift.io/v1")
+		assert.Contains(t, content, "kind: Route")
+		assert.Contains(t, content, "default.host")
+		assert.Contains(t, content, "internal.host")
+		assert.Contains(t, content, "external.host")
+	})
+
+	t.Run("skipped-ext", func(t *testing.T) {
+		obj := internal.GenerateObj(`apiVersion: route.openshift.io/v1
+kind: Route
+metadata:
+  name: my-app-api-ext`)
+		processed, template, err := testInstance.Process(&metadata.Service{}, obj)
+		assert.NoError(t, err)
+		assert.Equal(t, true, processed)
+		assert.Nil(t, template)
+	})
+
+	t.Run("skipped-non-route", func(t *testing.T) {
+		obj := internal.TestNs
+		processed, template, err := testInstance.Process(&metadata.Service{}, obj)
+		assert.NoError(t, err)
+		assert.Equal(t, false, processed)
+		assert.Nil(t, template)
+	})
+}
