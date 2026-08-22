b86fd19d8bd26e306a4400bd53569029c2434a91
Danilo Nicioka <danilo.nicioka@tjpa.jus.br>
2026-08-22 12:27:47 -0300

    refactor: add toMapStringInterface helper to support diverse map types in mergeDevValues

--- pkg/helm/dev_values.go
+++ pkg/helm/dev_values.go
@@ -2,11 +2,29 @@ package helm
 
 import (
 	"bytes"
+	"fmt"
 	"strings"
 
 	"github.com/danilonicioka/helmify/pkg/helmify"
 	"gopkg.in/yaml.v3"
 )
+
+func toMapStringInterface(v interface{}) (map[string]interface{}, bool) {
+	if m, ok := v.(map[string]interface{}); ok {
+		return m, true
+	}
+	if m, ok := v.(helmify.Values); ok {
+		return map[string]interface{}(m), true
+	}
+	if m, ok := v.(map[interface{}]interface{}); ok {
+		res := make(map[string]interface{})
+		for k, val := range m {
+			res[fmt.Sprintf("%v", k)] = val
+		}
+		return res, true
+	}
+	return nil, false
+}
 
 // mergeDevValues injects the 'cm' blocks from the dynamically generated values
 // into the static values-ca.yaml node tree to preserve comments and structure.
@@ -21,9 +39,9 @@ func mergeDevValues(caData []byte, chartName string, values helmify.Values) ([]b
 	}
 
 	for k, v := range values {
-		if valMap, ok := v.(map[string]interface{}); ok {
+		if valMap, ok := toMapStringInterface(v); ok {
 			if cmVal, ok := valMap["cm"]; ok {
-				if cmMap, ok := cmVal.(map[string]interface{}); ok {
+				if cmMap, ok := toMapStringInterface(cmVal); ok {
 					if len(cmMap) > 0 {
 						if err := mergeYamlNode(&node, cmMap, []string{k, "cm"}); err != nil {
 							return nil, err
@@ -32,9 +50,9 @@ func mergeDevValues(caData []byte, chartName string, values helmify.Values) ([]b
 				}
 			}
 			if filesVal, ok := valMap["files"]; ok {
-				if filesMap, ok := filesVal.(map[string]interface{}); ok {
+				if filesMap, ok := toMapStringInterface(filesVal); ok {
 					if filesCmVal, ok := filesMap["cm"]; ok {
-						if filesCmMap, ok := filesCmVal.(map[string]interface{}); ok {
+						if filesCmMap, ok := toMapStringInterface(filesCmVal); ok {
 							if len(filesCmMap) > 0 {
 								if err := mergeYamlNode(&node, filesCmMap, []string{k, "files", "cm"}); err != nil {
 									return nil, err
